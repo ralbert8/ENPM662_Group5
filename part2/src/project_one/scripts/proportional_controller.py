9aacf6bc044db929c85c8f02def960e34fd38c67
Carissa Arillo <carillo@umd.edu>
2024-10-28 23:37:30 -0400

    closed loop controller operational but kinematic eqs are incorrect. odom and velocity publisher/subscriber are working as well

--- part2/src/project_one/scripts/proportional_controller.py
+++ part2/src/project_one/scripts/proportional_controller.py
@@ -12,10 +12,9 @@ import time
 import matplotlib.pyplot as plt
 import math
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
-from nav_msgs.msg import Odometry
 from tf_transformations import euler_from_quaternion
-
-
+from rclpy.executors import MultiThreadedExecutor
+from geometry_msgs.msg import PoseStamped
 
 class ProportionalControlNode(Node):
 
@@ -23,32 +22,13 @@ class ProportionalControlNode(Node):
         
         super().__init__('proportional_control_node')
         
-        # Timer to send goals every 2 seconds (if list is not empty)
-        self._timer = self.create_timer(1.0, self.timer_callback)
-                            
-        #Publishers
-        self.joint_position_pub   = self.create_publisher(Float64MultiArray, '/position_controller/commands', 10)
-        self.wheel_velocities_pub = self.create_publisher(Float64MultiArray, '/velocity_controller/commands', 10)
-        self.joint_state_pub      = self.create_publisher(JointState, '/joint_states', 10) # TODO what is this for?
-        
-        # Subscriber to get odometry data
-        # We are using a MutuallyExclusiveCallbackGroup to ensure
-        # that the callback is executed in a separate thread from the main thread
-        # qos_profile = QoSProfile(depth=10)
-        self._odometry_cbg = MutuallyExclusiveCallbackGroup()
-        self._odometry_sub = self.create_subscription(
-            Odometry,
-            "odom",
-            self.odometry_cb,
-            10,
-            callback_group=self._odometry_cbg,
-        )
-
         # Initialize variables
         self._goal_x       = 10.0 * 39.37 
         self._goal_y       = 10.0 * 39.37 
         self._robot_x      = 0.0
         self._robot_y      = 0.0
+        self._robot_xdot   = 0.0
+        self._robot_ydot   = 0.0        
         self._robot_yaw    = 0.0
         self._goal_reached = False
         
@@ -62,29 +42,70 @@ class ProportionalControlNode(Node):
         # Proportional control gains
         self._kl = 0.5  # Linear velocity gain
         self._ka = 0.8  # Angular velocity gain
+
+        # Timer to send goals every 2 seconds (if list is not empty)
+        self._timer = self.create_timer(0.3, self.timer_callback)
+          
+        # Subscriber to get position data        
+        self._mutex_cbg = MutuallyExclusiveCallbackGroup() # MutuallyExclusiveCallbackGroup to ensure callback in separate thread
+        self._pose_sub = self.create_subscription(
+            PoseStamped,
+            "odom",
+            self.pose_cb,
+            10,
+            callback_group=self._mutex_cbg,
+        )
+        self._vel_sub = self.create_subscription(
+            Twist,
+            "velocity",
+            self.vel_cb,
+            10,
+            callback_group=self._mutex_cbg,
+        )        
+                          
+        #Publishers
+        self.joint_position_pub   = self.create_publisher(Float64MultiArray, '/position_controller/commands', 10)
+        self.wheel_velocities_pub = self.create_publisher(Float64MultiArray, '/velocity_controller/commands', 10)
+        self.joint_state_pub      = self.create_publisher(JointState, '/joint_states', 10) # TODO what is this for?
+                        
         
-    def odometry_cb(self, msg: Odometry):
+    def pose_cb(self, msg: PoseStamped):
         """
-        Callback to handle odometry data
+        Callback to handle pose data
 
         Args:
-            msg (Odometry): Odometry message
+            msg (PoseStamped): PoseStamped message
         """
-        
         # position
-        self._robot_x = msg.pose.pose.position.x
-        self._robot_y = msg.pose.pose.position.y
+        self._robot_x = msg.pose.position.x
+        self._robot_y = msg.pose.position.y        
+        
+        self.get_logger().info(f"Received data on robot pose-x and pose-y: {self._robot_x}, {self._robot_y}")
         
         # orientation
         quaternion = (
-            msg.pose.pose.orientation.x,
-            msg.pose.pose.orientation.y,
-            msg.pose.pose.orientation.z,
-            msg.pose.pose.orientation.w,
+            msg.pose.orientation.x,
+            msg.pose.orientation.y,
+            msg.pose.orientation.z,
+            msg.pose.orientation.w,
         )
         
         # Convert quaternion to Euler angles
         _, _, self._robot_yaw = euler_from_quaternion(quaternion)
+        
+    def vel_cb(self, msg: Twist):
+        """
+        Callback to handle velocity data
+
+        Args:
+            msg (Twist): Twist message
+        """
+        # velocity
+        self._robot_xdot = msg.linear.x
+        self._robot_ydot = msg.linear.y
+        
+        self.get_logger().info(f"Received data on robot velocity-x and velocity-y: {self._robot_xdot}, {self._robot_ydot}")        
+        
 
     def timer_callback(self):
         """
@@ -96,11 +117,10 @@ class ProportionalControlNode(Node):
             return 
 
         self.get_logger().info(f"Executing goal: ({self._goal_x},{self._goal_y})")
-        # Create a rate object to control the loop rate
-        rate = self.create_rate(10)
 
         while rclpy.ok():  # While ROS is running
             distance_to_goal = self.compute_distance_to_goal()
+            print("Distance to goal: ",distance_to_goal)
 
             if distance_to_goal < 0.02:
                 self.get_logger().info("Goal reached!")
@@ -109,7 +129,7 @@ class ProportionalControlNode(Node):
                 return 
 
             self.adjust_robot_motion(distance_to_goal)
-            rate.sleep()
+
                         
     def compute_distance_to_goal(self):
         """
@@ -177,49 +197,53 @@ class ProportionalControlNode(Node):
             distance_to_goal (float): Distance to the goal
 
         """
-        dy = self._goal_y - self._robot_y
-        dx = self._goal_x - self._robot_x
+
         # Compute angle to goal
+        x             = self._robot_x    
+        y             = self._robot_y
+        x_dot         = self._robot_xdot
+        y_dot         = self._robot_ydot       
+        dy            = self._goal_y - y
+        dx            = self._goal_x - x       
         angle_to_goal = math.atan2(dy, dx)
+        
+        print("Robot angle to goal : ",angle_to_goal)
+        print("Robot position X: ",x)
+        print("Robot position Y: ",y)
+        print("Robot velocity X: ",x_dot)
+        print("Robot velocity Y: ",y_dot)        
+                
         # Compute angular velocity
         angular_velocity = self.calculate_angular_velocity(angle_to_goal)
-        # Proportional control for linear velocity
-        linear_velocity = min(self._kl * distance_to_goal, 0.6)
+        print("Desired steering angular velocity: ",angular_velocity)
         
         # Set car params [inches]
         L       = 38
-        r_wheel = 8/2
-        
+        r_wheel = 8/2        
+                                
         # kinematic equations
-        # lin_vel_right_wheel = float((1 / r_wheel) * (math.sqrt(x_dot_goal[i]**2 + y_dot_goal[i]**2) + (L / 2) * theta_dot_goal[i]))
-        # lin_vel_left_wheel  = float((1 / r_wheel) * (math.sqrt(x_dot_goal[i]**2 + y_dot_goal[i]**2) - (L / 2) * theta_dot_goal[i]))
-        # steer_angle = np.arctan( L * theta_dot_goal[i] / math.sqrt(x_dot_goal[i]**2 + y_dot_goal[i]**2)   )
+        lin_vel_right_wheel = float( (1 / r_wheel) * (math.sqrt(x_dot**2 + y_dot**2) + (L / 2) * angular_velocity)   ) #- (self._kl * distance_to_goal)
+        lin_vel_left_wheel  = float( (1 / r_wheel) * (math.sqrt(x_dot**2 + y_dot**2) - (L / 2) * angular_velocity)   ) #- (self._kl * distance_to_goal)
+        if x_dot == 0 and y_dot == 0:
+            steer_angle = 0
+        else:
+            steer_angle = np.arctan( L * angular_velocity / math.sqrt(x_dot**2 + y_dot**2)   )
         
         # track commanded velocity & steering angle
-        self.steer_angles.append(angular_velocity)  #(steer_angle)
-        self.right_vels.append(linear_velocity)  #(lin_vel_right_wheel)
-        self.left_vels.append(linear_velocity)  #(lin_vel_left_wheel)
+        # self.steer_angles.append(steer_angle)
+        # self.right_vels.append(lin_vel_right_wheel)
+        # self.left_vels.append(lin_vel_left_wheel)        
 
-        # print("Commanded Steering Angle: ",steer_angle)
-        # print("Commanded Right & Left Wheel Linear Velocity: ",-lin_vel_right_wheel,", ",lin_vel_left_wheel)
-        # # Publish the twist message
-        # wheel_velocities.data = [-lin_vel_left_wheel,lin_vel_right_wheel]
-        # joint_positions.data  = [steer_angle,steer_angle]
-        # self.joint_position_pub.publish(joint_positions)
-        # self.wheel_velocities_pub.publish(wheel_velocities)
-        
-        print("Commanded Steering Angle: ",angular_velocity)
-        print("Commanded Right & Left Wheel Linear Velocity: ",-linear_velocity,", ",linear_velocity)
+        print("Commanded Steering Angle: ",steer_angle)
+        print("Commanded Right & Left Wheel Linear Velocity: ",-lin_vel_right_wheel,", ",lin_vel_left_wheel)
         # Publish the twist message
-        self.wheel_velocities.data = [-linear_velocity,linear_velocity]
-        self.joint_positions.data  = [angular_velocity,angular_velocity]
+        self.wheel_velocities.data = [-lin_vel_left_wheel,lin_vel_right_wheel]
+        self.joint_positions.data  = [steer_angle,steer_angle]
         self.joint_position_pub.publish(self.joint_positions)
-        self.wheel_velocities_pub.publish(self.wheel_velocities)        
-
+        self.wheel_velocities_pub.publish(self.wheel_velocities)
+        
 
         
-    
-    
     def prop_control(self):
 
         # Circular trajectory constraints
@@ -328,6 +352,17 @@ def main(args=None):
     node = ProportionalControlNode()
     #steer_angles, right_vels, left_vels, x_goal, y_goal, t = node.prop_control()
     
+    executor = MultiThreadedExecutor()
+    executor.add_node(node)
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        node.get_logger().info("Keyboard Interrupt (SIGINT) detected")
+    finally:
+        node.destroy_node()
+        # clean up the node
+        rclpy.shutdown()  
+            
     # # Plot commanded positions / velocities over time
     # plt.figure()
     # plt.subplot(2, 2, 1)
@@ -351,13 +386,7 @@ def main(args=None):
     # plt.grid()
     # plt.show()
     
-    try:
-        rclpy.spin(node) # spin the node
-    except KeyboardInterrupt:
-        node.get_logger().info("KeyboardInterrupt")
-    finally:
-        node.destroy_node()
-        rclpy.shutdown()
+      
     
     
 
