24fefffce52292cd55b3b3a753de3615e0ffc28c
ahall113 <162867442+ahall113@users.noreply.github.com>
2024-10-29 22:05:12 -0400

    Add files via upload
    Latest Rev

--- proportional_controller.py
+++ proportional_controller.py
@@ -43,9 +43,11 @@ class ProportionalControlNode(Node):
         #timer
         self._goal_x       = 10.0 * 39.37 
         self._goal_y       = 10.0 * 39.37
+        self._goal_theta   = math.atan2(self._goal_x, self._goal_y)
+        
         self._goal_reached = False
         self._timer = self.create_timer(0.3, self.timer_callback)
-
+        
 
     def odom_callback(self, msg: PoseStamped):
         # self.get_logger().info(f"Received ODOM data: {msg.data}")
@@ -71,16 +73,16 @@ class ProportionalControlNode(Node):
 
         self.get_logger().info(f"Executing goal: ({self._goal_x},{self._goal_y})")
 
-        while rclpy.ok():  # While ROS is running
-            distance_to_goal = self.compute_distance_to_goal()
-            print("Distance to goal: ",distance_to_goal)
-            if distance_to_goal < 39.37:
-                self.get_logger().info("Goal reached!")
-                self.stop_robot()
-                self._goal_reached = True
-                return 
+    
+        distance_to_goal = self.compute_distance_to_goal()
+        print("Distance to goal: ",distance_to_goal)
+        if distance_to_goal < 39.37:
+            self.get_logger().info("Goal reached!")
+            self.stop_robot()
+            self._goal_reached = True
+            return 
 
-            self.adjust_robot_motion(distance_to_goal)
+        self.adjust_robot_motion(distance_to_goal)
         
     def compute_distance_to_goal(self):
         """
@@ -89,25 +91,27 @@ class ProportionalControlNode(Node):
         Returns:
             float: Distance to the goal
         """
-        return math.sqrt(
-            (self._goal_x - self._robot_x) ** 2 + (self._goal_y - self._robot_y) ** 2
-        )
+        return math.sqrt((self._goal_x - self.actual_x) ** 2 + (self._goal_y - self.actual_y) ** 2)
         
-    def adjust_robot_motion(self):
-        
-        
-        
+    def adjust_robot_motion(self, distance_to_goal):
+        steer_limit = 75 # degress
+        joint_positions = Float64MultiArray()
+        wheel_velocities = Float64MultiArray()
+        # Car params [inches]
+        L       = 38  
+        r_wheel = 8/2
+        k_p = .2
         # ODOM Postion error calculations
         print("odom x: ", self.actual_x)
-        x_err = y_goal[i] - self.actual_x
+        x_err = self._goal_x - self.actual_x
         print("odom y: ", self.actual_y)
-        y_err = y_goal[i] - self.actual_y
-        lin_vel_err_mag = math.sqrt(x_err**2 + y_err**2)
+        y_err = self._goal_y - self.actual_y
+        # lin_vel_err_mag = math.sqrt(x_err**2 + y_err**2)
         print("odom theta: ", self.actual_theta)
-        theta_err = theta_goal[i] - self.actual_theta
+        theta_err = math.atan2(y_err,x_err) - self.actual_theta
         
-        prop_control_lin_vel = k_p * lin_vel_err_mag 
-        prop_control_ang_vel = k_p * theta_err
+        prop_control_lin_vel = k_p * distance_to_goal 
+        prop_control_ang_vel = .6 * theta_err
         
         # lin_vel_right_wheel = float((1 / r_wheel) * (math.sqrt(x_dot_goal[i]**2 + y_dot_goal[i]**2) + (L / 2) * theta_dot_goal[i]))
         # lin_vel_left_wheel  = float((1 / r_wheel) * (math.sqrt(x_dot_goal[i]**2 + y_dot_goal[i]**2) - (L / 2) * theta_dot_goal[i]))
@@ -115,15 +119,19 @@ class ProportionalControlNode(Node):
         lin_vel_right_wheel = float((1 / r_wheel) * (prop_control_lin_vel + (L / 2) * prop_control_ang_vel))
         lin_vel_left_wheel  = float((1 / r_wheel) * (prop_control_lin_vel - (L / 2) * prop_control_ang_vel))
                     
-        linear_vel  = (1/2) * (  x_dot_goal[i]  / np.cos(theta_goal[i]) / (r_wheel/2)  )
-        steer_angle = np.arctan( L * theta_dot_goal[i] / math.sqrt(x_dot_goal[i]**2 + y_dot_goal[i]**2) )
-        
-        steer_angles.append(steer_angle)
-        linear_vels1.append(lin_vel_right_wheel)
-        linear_vels2.append(lin_vel_left_wheel)
-
-        print("Steer Angle",steer_angle)
-        print("Linear Velocity",-lin_vel_right_wheel,", ",lin_vel_left_wheel)
+        # linear_vel  = (1/2) * (  x_dot_goal[i]  / np.cos(theta_goal[i]) / (r_wheel/2)  )
+        # steer_angle = np.arctan( L * theta_dot_goal[i] / math.sqrt(x_dot_goal[i]**2 + y_dot_goal[i]**2) )
+        steer_angle = math.atan2(2*L*math.sin(theta_err), distance_to_goal)
+        if steer_angle> math.radians(steer_limit):
+            steer_angle=math.radians(steer_limit)
+        if steer_angle<-math.radians(steer_limit):
+            steer_angle=-math.radians(steer_limit)
+            
+        # steer_angles.append(steer_angle)
+        # linear_vels1.append(lin_vel_right_wheel)
+        # linear_vels2.append(lin_vel_left_wheel)
+        # print("Steer Angle",steer_angle)
+        # print("Linear Velocity",-lin_vel_right_wheel,", ",lin_vel_left_wheel)
         # Publish the twist message
         wheel_velocities.data = [-lin_vel_left_wheel,lin_vel_right_wheel] 
         joint_positions.data = [steer_angle,steer_angle] 
@@ -131,94 +139,20 @@ class ProportionalControlNode(Node):
         self.joint_position_pub.publish(joint_positions)
         self.wheel_velocities_pub.publish(wheel_velocities)
         
-        #car stops when within one car length of 10,10 gridspace
-        # if abs(x_err) < L and abs(y_err) < L:
-        #     break
         
-    def prop_control(self):
-
-        # Circular trajectory constraints
-        r        = 10 * 39.37   # radius of circle [cm]
-        duration = 30   # duration of motion [sec]
-        dt       = 1    # sample interval [sec]
-
-        # Calculate goal points to make circular trajectory
-        # using polar coordinates to define eq of circle
-        t              = [0]
-        x_goal         = [0]
-        y_goal         = [0]
-        theta_goal     = [0]
-        x_dot_goal     = [0]
-        y_dot_goal     = [0]
-        theta_dot_goal = [0]                
-        i              = 0
-
-        # gather goal points along trajectoy
-        while i < duration:
-            i += dt
-            print("i: ",i)
-            
-            # Assume car will travel 1/4 circle --> 2pi = full circle --> 1/4 circle = pi/2
-            theta      = (-(np.pi / 2)) + (np.pi / 2) * ( i     / duration) #integrated chunk of circle based on time - RADIANS
-            theta_prev = (-(np.pi / 2)) + (np.pi / 2) * ((i-dt) / duration)
-            
-            t.append(i)
-            theta_goal.append(theta)
-            
-            x = r * np.cos(theta)       #polar coords for eq of circle
-            y = r * np.sin(theta) + r   #offset to start at 0,0 and travel to 10,10
-            x_goal.append(x)   
-            y_goal.append(y)
-            
-            theta_dot = (theta - theta_prev) / dt            
-            theta_dot_goal.append(theta_dot)
-            
-            x_dot_goal.append(-y * theta_dot)
-            y_dot_goal.append( x * theta_dot)
-                    
-        # Command the car    
-        self.msg = "Closed Loop Proportion Controller"
-        self.get_logger().info(self.msg)
-        joint_positions  = Float64MultiArray()  #front-axle steering
-        wheel_velocities = Float64MultiArray() #rear-wheel drive
-        linear_vel       = 0.0 #initial value
-        steer_angle      = 0.0 #initial value
-        steer_angles     = [0]
-        linear_vels1      = [0]
-        linear_vels2      = [0]
-        k_p = .2 #proportional gain
-        x_err_list = [0]
-        y_err_list = [0]
-        theta_err_list = [0]
-        # Car params [inches]
-        L       = 38  
-        r_wheel = 8/2
+    def stop_robot(self):
+        """
+        Stop the robot by setting velocities and angles to zero
+        """
+        self.joint_position_pub.publish(Float64MultiArray(data=[0.0, 0.0]))
+        self.wheel_velocities_pub.publish(Float64MultiArray(data=[0.0, 0.0]))
         
-        i = 0
-        while i < (duration / dt):
-            
-
-        print("Duration Complete. Stopping Robot")
-        steer_angle=0.0
-        linear_vel=0.0
-        print("Steer Angle",steer_angle)
-        print("Linear Velocity",linear_vel)
-        
-        # Publish the twist message
-        wheel_velocities.data = [-linear_vel,linear_vel] # need to update with new variable names
-        joint_positions.data = [steer_angle,steer_angle] # need to update with new variable names
-
-        self.joint_position_pub.publish(joint_positions)
-        self.wheel_velocities_pub.publish(wheel_velocities)
-        
-        return steer_angles, linear_vels1, linear_vels2, x_goal, y_goal, t
-            
 def main(args=None):
     # Start Node and Controller
     print("hi")
     rclpy.init(args=args)
     node = ProportionalControlNode()
-    steer_angles, right_vels, left_vels, x_goal, y_goal, t = node.prop_control()
+    # steer_angles, right_vels, left_vels, x_goal, y_goal, t = node.prop_control()
     
     executor = MultiThreadedExecutor()
     executor.add_node(node)
@@ -230,7 +164,7 @@ def main(args=None):
         node.destroy_node()
         # clean up the node
         rclpy.shutdown()
-        
+       
     # rclpy.init(args=args)
     # node = ProportionalControlNode()
     # steer_angles, linear_vels1, linear_vels2, x_goal, y_goal, t = node.prop_control()
@@ -264,13 +198,9 @@ def main(args=None):
     # node.destroy_node()
     # rclpy.shutdown()
     
-    
+   
 
 if __name__ == '__main__':
     main()
     
 
-# #equations used from lecture
-# x_dot = (r_wheel/2) * (lin_vel_left_wheel + lin_vel_right_wheel) * math.cos(theta) 
-# y_dot = (r_wheel/2) * (lin_vel_left_wheel + lin_vel_right_wheel) * math.sin(theta)
-# theta_dot = (r_wheel/L) * (lin_vel_right_wheel - lin_vel_left_wheel)
