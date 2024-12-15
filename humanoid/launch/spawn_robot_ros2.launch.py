1de246520861a88498078a568284af38091b8fb2
Riley Albert <ralbert8@umd.edu>
2024-12-14 16:33:16 -0800

    Resolved Link Orientation Issues

--- humanoid/launch/spawn_robot_ros2.launch.py
+++ humanoid/launch/spawn_robot_ros2.launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
 
     # Position and orientation
     # [X, Y, Z]
-    position = [0.0, 0.0, 0]
+    position = [0.0, 0.0, 0.01]
     # [Roll, Pitch, Yaw]
     orientation = [0.0, 0.0, 0.0]
     # Base Name or robot
